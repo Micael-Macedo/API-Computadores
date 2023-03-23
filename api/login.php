3bb38e4e363119896f108f05a517b3f1250ba122
Micael Macedo <micaelm2009@hotmail.com>
2023-03-23 19:25:10 -0300

    fix: user authentication

--- api/login.php
+++ api/login.php
@@ -10,24 +10,31 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     require __DIR__.'/jwt/senha.php';
     $body = json_decode(file_get_contents('php://input'));
     $jwt = new MyJWT();
+    $token = $_COOKIE['token'];
     if($body->username == "" or $body->password == ""){
         http_response_code(422);
         $json = (object) array('Message' => 'Credenciais inválidas');
         echo json_encode($json);
     }else{
-        if($_COOKIE['token'] != ""){
-            http_response_code(403);
-            $json = (object) array('Message' => 'Usuário já autenticado');
+        if($token != ""){
+            $userDB = $jwt->decode($_COOKIE['token'],SENHA);
+            if($userDB["username"] == $body->username and $userDB["password"] == $body->password){
+                http_response_code(403);
+                $json = (object) array('Message' => 'Usuário já autenticado');
+                echo json_encode($json);
+            }else{
+            http_response_code(200);
+            $json = (object) array('token' => $jwt->encode((array)$body, SENHA));
+            setcookie('token', $json->token);
             echo json_encode($json);
+        }
         }else{
             http_response_code(200);
             $json = (object) array('token' => $jwt->encode((array)$body, SENHA));
             setcookie('token', $json->token);
             echo json_encode($json);
-            echo $_COOKIE['token'];
         }
     }
-
 }
 if($_SERVER['REQUEST_METHOD'] == "PUT"){
     $body = json_decode(file_get_contents('php://input'));
@@ -37,4 +44,3 @@ if($_SERVER['REQUEST_METHOD'] == "DELETE"){
     $body = json_decode(file_get_contents('php://input'));
     echo $body;
 }
-?>
