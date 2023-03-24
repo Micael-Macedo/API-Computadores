b34bc7507105057452a5b3363a8ce9e4af0956fa
Micael Macedo <micaelm2009@hotmail.com>
2023-03-24 19:02:12 -0300

    feat: add images url

--- api/images.php
+++ api/images.php
@@ -1,21 +1,19 @@
 <?php
-
-$url = $_SERVER['REQUEST_URI'];
-$header = getallheaders();
-if($_SERVER['REQUEST_METHOD'] == "GET"){
-    $body = json_decode(file_get_contents('php://input'));
-    echo $body;
-}
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $body = json_decode(file_get_contents('php://input'));
-    echo $body;
-}
-if($_SERVER['REQUEST_METHOD'] == "PUT"){
-    $body = json_decode(file_get_contents('php://input'));
-    echo $body;
-}
-if($_SERVER['REQUEST_METHOD'] == "DELETE"){
-    $body = json_decode(file_get_contents('php://input'));
-    echo $body;
-}
+require 'db/index.php';
+    $url = $_SERVER['REQUEST_URI'];
+    if($jwt->decode($token, SENHA)){
+        if($_SERVER['REQUEST_METHOD'] == "GET"){
+            $body = json_decode(file_get_contents('php://input'));
+            if($body){
+                $result = buscarItem("image", $body->id);
+                if($result != "null"){
+                    http_response_code(200);
+                    echo $result;
+                }else{
+                    http_response_code(404);
+                    echo json_encode((object) ['message'=> 'Imagem não encontrada']);
+                }
+            }
+        }
+    }
 ?>
