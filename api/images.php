1a8d2be7fef88f7a7d5c178697a296fc46764d14
Micael Macedo <micaelm2009@hotmail.com>
2023-03-24 19:04:00 -0300

    feat: remove authentication to images

--- api/images.php
+++ api/images.php
@@ -1,19 +1,17 @@
 <?php
 require 'db/index.php';
-    $url = $_SERVER['REQUEST_URI'];
-    if($jwt->decode($token, SENHA)){
-        if($_SERVER['REQUEST_METHOD'] == "GET"){
-            $body = json_decode(file_get_contents('php://input'));
-            if($body){
-                $result = buscarItem("image", $body->id);
-                if($result != "null"){
-                    http_response_code(200);
-                    echo $result;
-                }else{
-                    http_response_code(404);
-                    echo json_encode((object) ['message'=> 'Imagem não encontrada']);
-                }
-            }
+$url = $_SERVER['REQUEST_URI'];
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+    $body = json_decode(file_get_contents('php://input'));
+    if($body){
+        $result = buscarItem("image", $body->id);
+        if($result != "null"){
+            http_response_code(200);
+            echo $result;
+        }else{
+            http_response_code(404);
+            echo json_encode((object) ['message'=> 'Imagem não encontrada']);
         }
     }
+}
 ?>
